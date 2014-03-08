4a78865a350b513119d6af8d61a3c4ba1c5c7713
Guido Rota <guido.rota@gmail.com>
2014-03-08 19:44:15 +0100

    Division operand implementation and testing

--- src/compiler/codegen_expression.c
+++ src/compiler/codegen_expression.c
@@ -50,6 +50,10 @@ static struct bx_comp_expr *multiplication_operator(struct bx_comp_expr *operand
 static struct bx_comp_expr *mul_int(struct bx_comp_expr *operand1, struct bx_comp_expr *operand2);
 static struct bx_comp_expr *mul_float(struct bx_comp_expr *operand1, struct bx_comp_expr *operand2);
 
+static struct bx_comp_expr *division_operator(struct bx_comp_expr *operand1, struct bx_comp_expr *operand2);
+static struct bx_comp_expr *div_int(struct bx_comp_expr *operand1, struct bx_comp_expr *operand2);
+static struct bx_comp_expr *div_float(struct bx_comp_expr *operand1, struct bx_comp_expr *operand2);
+
 static struct bx_comp_expr *int_to_float(struct bx_comp_expr *value);
 static struct bx_comp_expr *float_to_int(struct bx_comp_expr *value);
 static struct bx_comp_expr *constant_to_binary(struct bx_comp_expr *value);
@@ -151,6 +155,9 @@ struct bx_comp_expr *bx_cgex_expression(struct bx_comp_expr *operand1,
 		break;
 	case BX_COMP_MUL:
 		return multiplication_operator(operand1, operand2);
+		break;
+	case BX_COMP_DIV:
+		return division_operator(operand1, operand2);
 		break;
 	default:
 		return NULL;
@@ -403,6 +410,87 @@ static struct bx_comp_expr *mul_float(struct bx_comp_expr *operand1, struct bx_c
 	bx_cgco_append_code(result->bx_value.code, operand1->bx_value.code);
 	bx_cgco_append_code(result->bx_value.code, operand2->bx_value.code);
 	bx_cgco_add_instruction(result->bx_value.code, BX_INSTR_FMUL);
+
+	return result;
+}
+
+///////////////////////
+// DIVISION OPERATOR //
+///////////////////////
+
+static struct bx_comp_expr *division_operator(struct bx_comp_expr *operand1, struct bx_comp_expr *operand2) {
+
+	if (operand1->data_type == BX_BOOL || operand2->data_type == BX_BOOL ||
+			operand1->data_type == BX_SUBNET || operand2->data_type == BX_SUBNET ||
+			operand1->data_type == BX_STREAM || operand2->data_type == BX_STREAM ||
+			operand1->data_type == BX_STRING || operand2->data_type == BX_STRING) {
+		BX_LOG(LOG_ERROR, "codegen_expression", "Operands not compatible with operator '-'.");
+		return NULL;
+
+	} else if (operand1->data_type == BX_INT && operand2->data_type == BX_INT) {
+		return div_int(operand1, operand2);
+
+	} else if (operand1->data_type == BX_FLOAT && operand2->data_type == BX_FLOAT) {
+		return div_float(operand1, operand2);
+
+	} else if (operand1->data_type == BX_INT && operand2->data_type == BX_FLOAT) {
+		operand1 = int_to_float(operand1);
+		return div_float(operand1, operand2);
+
+	} else if (operand2->data_type == BX_INT && operand1->data_type == BX_FLOAT) {
+		operand2 = int_to_float(operand2);
+		return div_float(operand1, operand2);
+
+	} else {
+		BX_LOG(LOG_ERROR, "codegen_expression", "Unexpected operand type in function plus_operator.");
+		return NULL;
+	}
+}
+
+static struct bx_comp_expr *div_int(struct bx_comp_expr *operand1, struct bx_comp_expr *operand2) {
+	struct bx_comp_expr *result;
+
+	if (operand1->type == BX_COMP_CONSTANT && operand2->type == BX_COMP_CONSTANT) {
+		return bx_cgex_create_int_constant(operand1->bx_value.int_value / operand2->bx_value.int_value);
+	}
+
+	if (operand1->type == BX_COMP_CONSTANT) {
+		operand1 = constant_to_binary(operand1);
+	}
+
+	if (operand2->type == BX_COMP_CONSTANT) {
+		operand2 = constant_to_binary(operand2);
+	}
+
+	result = create_code_expression();
+	result->data_type = BX_INT;
+	bx_cgco_append_code(result->bx_value.code, operand1->bx_value.code);
+	bx_cgco_append_code(result->bx_value.code, operand2->bx_value.code);
+	bx_cgco_add_instruction(result->bx_value.code, BX_INSTR_IDIV);
+
+	return result;
+}
+
+static struct bx_comp_expr *div_float(struct bx_comp_expr *operand1, struct bx_comp_expr *operand2) {
+	struct bx_comp_expr *result;
+
+	if (operand1->type == BX_COMP_CONSTANT && operand2->type == BX_COMP_CONSTANT) {
+		return bx_cgex_create_float_constant(operand1->bx_value.float_value / operand2->bx_value.float_value);
+	}
+
+	if (operand1->type == BX_COMP_CONSTANT) {
+		operand1 = constant_to_binary(operand1);
+	}
+
+	if (operand2->type == BX_COMP_CONSTANT) {
+		operand2 = constant_to_binary(operand2);
+	}
+
+	result = create_code_expression();
+	result->data_type = BX_FLOAT;
+	bx_cgco_append_code(result->bx_value.code, operand1->bx_value.code);
+	bx_cgco_append_code(result->bx_value.code, operand2->bx_value.code);
+	bx_cgco_add_instruction(result->bx_value.code, BX_INSTR_FDIV);
 
 	return result;
 }
--- test/compiler/test_codegen_expression.c
+++ test/compiler/test_codegen_expression.c
@@ -351,7 +351,6 @@ START_TEST (subtraction_operator) {
 	bx_cgex_destroy_expression(operand2);
 	bx_cgex_destroy_expression(result);
 
-	//TODO: Continue here
 	// Operand1 Int, Operand2 Float, constant
 	operand1 = bx_cgex_create_int_constant(int_operand1);
 	ck_assert_ptr_ne(operand1, NULL);
@@ -542,7 +541,6 @@ START_TEST (multiplication_operator) {
 	bx_cgex_destroy_expression(operand2);
 	bx_cgex_destroy_expression(result);
 
-	//TODO: Continue here
 	// Operand1 Int, Operand2 Float, constant
 	operand1 = bx_cgex_create_int_constant(int_operand1);
 	ck_assert_ptr_ne(operand1, NULL);
@@ -694,6 +692,195 @@ START_TEST (multiplication_operator) {
 	bx_cgex_destroy_expression(result);
 } END_TEST
 
+START_TEST (division_operator) {
+	bx_int8 error;
+	struct bx_comp_expr *operand1;
+	struct bx_comp_expr *operand2;
+	struct bx_comp_expr *result;
+	bx_int32 int_operand1 = 34;
+	bx_int32 int_operand2 = -7;
+	bx_float32 float_operand1 = 2;
+	bx_float32 float_operand2 = 28.87;
+
+	// Int operand, constant
+	operand1 = bx_cgex_create_int_constant(int_operand1);
+	ck_assert_ptr_ne(operand1, NULL);
+	operand2 = bx_cgex_create_int_constant(int_operand2);
+	ck_assert_ptr_ne(operand1, NULL);
+	result = bx_cgex_expression(operand1, operand2, BX_COMP_DIV);
+	ck_assert_ptr_ne(result, NULL);
+	ck_assert_int_eq(result->type, BX_COMP_CONSTANT);
+	ck_assert_int_eq(result->data_type, BX_INT);
+	ck_assert_int_eq(result->bx_value.int_value, int_operand1 / int_operand2);
+	bx_cgex_destroy_expression(operand1);
+	bx_cgex_destroy_expression(operand2);
+	bx_cgex_destroy_expression(result);
+
+	// Float operand, constant
+	operand1 = bx_cgex_create_float_constant(float_operand1);
+	ck_assert_ptr_ne(operand1, NULL);
+	operand2 = bx_cgex_create_float_constant(float_operand2);
+	ck_assert_ptr_ne(operand1, NULL);
+	result = bx_cgex_expression(operand1, operand2, BX_COMP_DIV);
+	ck_assert_ptr_ne(result, NULL);
+	ck_assert_int_eq(result->type, BX_COMP_CONSTANT);
+	ck_assert_int_eq(result->data_type, BX_FLOAT);
+	ck_assert_int_eq(result->bx_value.float_value, float_operand1 / float_operand2);
+	bx_cgex_destroy_expression(operand1);
+	bx_cgex_destroy_expression(operand2);
+	bx_cgex_destroy_expression(result);
+
+	// Operand1 Int, Operand2 Float, constant
+	operand1 = bx_cgex_create_int_constant(int_operand1);
+	ck_assert_ptr_ne(operand1, NULL);
+	operand2 = bx_cgex_create_float_constant(float_operand2);
+	ck_assert_ptr_ne(operand1, NULL);
+	result = bx_cgex_expression(operand1, operand2, BX_COMP_DIV);
+	ck_assert_ptr_ne(result, NULL);
+	ck_assert_int_eq(result->type, BX_COMP_CONSTANT);
+	ck_assert_int_eq(result->data_type, BX_FLOAT);
+	ck_assert_int_eq(result->bx_value.float_value, int_operand1 / float_operand2);
+	bx_cgex_destroy_expression(operand1);
+	bx_cgex_destroy_expression(operand2);
+	bx_cgex_destroy_expression(result);
+
+	// Operand1 Float, Operand2 Int, constant
+	operand1 = bx_cgex_create_float_constant(float_operand1);
+	ck_assert_ptr_ne(operand1, NULL);
+	operand2 = bx_cgex_create_int_constant(int_operand2);
+	ck_assert_ptr_ne(operand1, NULL);
+	result = bx_cgex_expression(operand1, operand2, BX_COMP_DIV);
+	ck_assert_ptr_ne(result, NULL);
+	ck_assert_int_eq(result->type, BX_COMP_CONSTANT);
+	ck_assert_int_eq(result->data_type, BX_FLOAT);
+	ck_assert_int_eq(result->bx_value.float_value, float_operand1 / int_operand2);
+	bx_cgex_destroy_expression(operand1);
+	bx_cgex_destroy_expression(operand2);
+	bx_cgex_destroy_expression(result);
+
+	// Operand1 Int, Operand2 Int, binary
+	bx_test_field_set_int(&int_test_field, int_operand1);
+	operand1 = bx_cgex_create_variable(INT_TEST_FIELD);
+	ck_assert_ptr_ne(operand1, NULL);
+	operand2 = bx_cgex_create_variable(INT_TEST_FIELD);
+	ck_assert_ptr_ne(operand2, NULL);
+	result = bx_cgex_expression(operand1, operand2, BX_COMP_DIV);
+	ck_assert_ptr_ne(result, NULL);
+	ck_assert_int_eq(result->data_type, BX_INT);
+	ck_assert_int_eq(result->type, BX_COMP_BINARY);
+	ck_assert_ptr_ne(result->bx_value.code, NULL);
+	bx_cgco_add_instruction(result->bx_value.code, BX_INSTR_STORE32);
+	bx_cgco_add_identifier(result->bx_value.code, INT_TEST_FIELD);
+	error = bx_vm_execute(result->bx_value.code->data, result->bx_value.code->size);
+	ck_assert_int_eq(error, 0);
+	ck_assert_int_eq(bx_test_field_get_int(&int_test_field), int_operand1 / int_operand1);
+	bx_cgex_destroy_expression(operand1);
+	bx_cgex_destroy_expression(operand2);
+	bx_cgex_destroy_expression(result);
+
+	// Operand1 Float, Operand2 Float, binary
+	bx_test_field_set_float(&float_test_field, float_operand1);
+	operand1 = bx_cgex_create_variable(FLOAT_TEST_FIELD);
+	ck_assert_ptr_ne(operand1, NULL);
+	operand2 = bx_cgex_create_variable(FLOAT_TEST_FIELD);
+	ck_assert_ptr_ne(operand2, NULL);
+	result = bx_cgex_expression(operand1, operand2, BX_COMP_DIV);
+	ck_assert_ptr_ne(result, NULL);
+	ck_assert_int_eq(result->data_type, BX_FLOAT);
+	ck_assert_int_eq(result->type, BX_COMP_BINARY);
+	ck_assert_ptr_ne(result->bx_value.code, NULL);
+	bx_cgco_add_instruction(result->bx_value.code, BX_INSTR_STORE32);
+	bx_cgco_add_identifier(result->bx_value.code, FLOAT_TEST_FIELD);
+	error = bx_vm_execute(result->bx_value.code->data, result->bx_value.code->size);
+	ck_assert_int_eq(error, 0);
+	ck_assert_int_eq(bx_test_field_get_float(&float_test_field), float_operand1 / float_operand1);
+	bx_cgex_destroy_expression(operand1);
+	bx_cgex_destroy_expression(operand2);
+	bx_cgex_destroy_expression(result);
+
+	// Operand1 Int, Operand2 Float, binary
+	bx_test_field_set_int(&int_test_field, int_operand1);
+	bx_test_field_set_float(&float_test_field, float_operand2);
+	operand1 = bx_cgex_create_variable(INT_TEST_FIELD);
+	ck_assert_ptr_ne(operand1, NULL);
+	operand2 = bx_cgex_create_variable(FLOAT_TEST_FIELD);
+	ck_assert_ptr_ne(operand2, NULL);
+	result = bx_cgex_expression(operand1, operand2, BX_COMP_DIV);
+	ck_assert_ptr_ne(result, NULL);
+	ck_assert_int_eq(result->data_type, BX_FLOAT);
+	ck_assert_int_eq(result->type, BX_COMP_BINARY);
+	ck_assert_ptr_ne(result->bx_value.code, NULL);
+	bx_cgco_add_instruction(result->bx_value.code, BX_INSTR_STORE32);
+	bx_cgco_add_identifier(result->bx_value.code, FLOAT_TEST_FIELD);
+	error = bx_vm_execute(result->bx_value.code->data, result->bx_value.code->size);
+	ck_assert_int_eq(error, 0);
+	ck_assert_int_eq(bx_test_field_get_float(&float_test_field), int_operand1 / float_operand2);
+	bx_cgex_destroy_expression(operand1);
+	bx_cgex_destroy_expression(operand2);
+	bx_cgex_destroy_expression(result);
+
+	// Operand1 Float, Operand2 Int, binary
+	bx_test_field_set_float(&float_test_field, float_operand1);
+	bx_test_field_set_int(&int_test_field, int_operand2);
+	operand1 = bx_cgex_create_variable(FLOAT_TEST_FIELD);
+	ck_assert_ptr_ne(operand1, NULL);
+	operand2 = bx_cgex_create_variable(INT_TEST_FIELD);
+	ck_assert_ptr_ne(operand2, NULL);
+	result = bx_cgex_expression(operand1, operand2, BX_COMP_DIV);
+	ck_assert_ptr_ne(result, NULL);
+	ck_assert_int_eq(result->data_type, BX_FLOAT);
+	ck_assert_int_eq(result->type, BX_COMP_BINARY);
+	ck_assert_ptr_ne(result->bx_value.code, NULL);
+	bx_cgco_add_instruction(result->bx_value.code, BX_INSTR_STORE32);
+	bx_cgco_add_identifier(result->bx_value.code, FLOAT_TEST_FIELD);
+	error = bx_vm_execute(result->bx_value.code->data, result->bx_value.code->size);
+	ck_assert_int_eq(error, 0);
+	ck_assert_int_eq(bx_test_field_get_float(&float_test_field), float_operand1 / int_operand2);
+	bx_cgex_destroy_expression(operand1);
+	bx_cgex_destroy_expression(operand2);
+	bx_cgex_destroy_expression(result);
+
+	// Operand1 Int constant, Operand2 Int binary
+	operand1 = bx_cgex_create_int_constant(int_operand1);
+	ck_assert_ptr_ne(operand1, NULL);
+	bx_test_field_set_int(&int_test_field, int_operand2);
+	operand2 = bx_cgex_create_variable(INT_TEST_FIELD);
+	ck_assert_ptr_ne(operand2, NULL);
+	result = bx_cgex_expression(operand1, operand2, BX_COMP_DIV);
+	ck_assert_ptr_ne(result, NULL);
+	ck_assert_int_eq(result->data_type, BX_INT);
+	ck_assert_int_eq(result->type, BX_COMP_BINARY);
+	ck_assert_ptr_ne(result->bx_value.code, NULL);
+	bx_cgco_add_instruction(result->bx_value.code, BX_INSTR_STORE32);
+	bx_cgco_add_identifier(result->bx_value.code, INT_TEST_FIELD);
+	error = bx_vm_execute(result->bx_value.code->data, result->bx_value.code->size);
+	ck_assert_int_eq(error, 0);
+	ck_assert_int_eq(bx_test_field_get_int(&int_test_field), int_operand1 / int_operand2);
+	bx_cgex_destroy_expression(operand1);
+	bx_cgex_destroy_expression(operand2);
+	bx_cgex_destroy_expression(result);
+
+	// Operand1 Float constant, Operand2 Float binary
+	operand1 = bx_cgex_create_float_constant(float_operand1);
+	ck_assert_ptr_ne(operand1, NULL);
+	bx_test_field_set_float(&float_test_field, float_operand2);
+	operand2 = bx_cgex_create_variable(FLOAT_TEST_FIELD);
+	ck_assert_ptr_ne(operand2, NULL);
+	result = bx_cgex_expression(operand1, operand2, BX_COMP_DIV);
+	ck_assert_ptr_ne(result, NULL);
+	ck_assert_int_eq(result->data_type, BX_FLOAT);
+	ck_assert_int_eq(result->type, BX_COMP_BINARY);
+	ck_assert_ptr_ne(result->bx_value.code, NULL);
+	bx_cgco_add_instruction(result->bx_value.code, BX_INSTR_STORE32);
+	bx_cgco_add_identifier(result->bx_value.code, FLOAT_TEST_FIELD);
+	error = bx_vm_execute(result->bx_value.code->data, result->bx_value.code->size);
+	ck_assert_int_eq(error, 0);
+	ck_assert_int_eq(bx_test_field_get_float(&float_test_field), float_operand1 / float_operand2);
+	bx_cgex_destroy_expression(operand1);
+	bx_cgex_destroy_expression(operand2);
+	bx_cgex_destroy_expression(result);
+} END_TEST
+
 Suite *test_codegen_expression_create_suite(void) {
 	Suite *suite = suite_create("bx_linked_list");
 	TCase *tcase;
@@ -720,6 +907,10 @@ Suite *test_codegen_expression_create_suite(void) {
 
 	tcase = tcase_create("multiplication_operator");
 	tcase_add_test(tcase, multiplication_operator);
+	suite_add_tcase(suite, tcase);
+
+	tcase = tcase_create("division_operator");
+	tcase_add_test(tcase, division_operator);
 	suite_add_tcase(suite, tcase);
 
 	return suite;
