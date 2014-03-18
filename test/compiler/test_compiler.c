d132c1bbbbe2ca7e9bea2fcce1ece891440deba3
Guido Rota <guido.rota@gmail.com>
2014-03-18 17:21:43 +0100

    Compiler tests

--- test/compiler/test_compiler.c
+++ test/compiler/test_compiler.c
@@ -70,6 +70,24 @@ static struct bx_test_field_data boolean_test_field_data;
 static struct bx_document_field boolean_output_test_field;
 static struct bx_test_field_data boolean_output_test_field_data;
 
+static void run_program(char *program) {
+	bx_int8 error;
+	int parse_result;
+	struct bx_comp_task *main_task;
+
+	main_task = bx_cgtk_create_task();
+	init_parser(main_task);
+	yyin = fmemopen(program, strlen(program), "r");
+	ck_assert_ptr_ne(yyin, NULL);
+	parse_result = yyparse();
+	ck_assert_int_eq(parse_result, 0);
+	fclose(yyin);
+
+	error = bx_vm_execute(main_task->code->data, main_task->code->size);
+	ck_assert_int_eq(error, 0);
+	bx_cgtk_destroy_task(main_task);
+}
+
 START_TEST (init_test) {
 	bx_int8 error;
 
@@ -151,6 +169,33 @@ START_TEST (assignment_test) {
 	bx_cgtk_destroy_task(main_task);
 } END_TEST
 
+START_TEST (expression_test) {
+	run_program("existing int int_test_field; int_test_field = 5 + 10;");
+	ck_assert_int_eq(bx_test_field_get_int(&int_test_field), 15);
+
+	run_program("existing int int_test_field; int_test_field = 5 * 10;");
+	ck_assert_int_eq(bx_test_field_get_int(&int_test_field), 50);
+
+	run_program("existing int int_test_field; int_test_field = 5 * 10 + 5;");
+	ck_assert_int_eq(bx_test_field_get_int(&int_test_field), 55);
+
+	run_program("existing int int_test_field; int_test_field = 5 * (10 + 5);");
+	ck_assert_int_eq(bx_test_field_get_int(&int_test_field), 75);
+
+	run_program("existing int int_test_field; int_test_field = 5 * (10 + 5);");
+	ck_assert_int_eq(bx_test_field_get_int(&int_test_field), 75);
+
+	run_program("existing int int_test_field; int_test_field = 5 * (11 / 5);");
+	ck_assert_int_eq(bx_test_field_get_int(&int_test_field), 10);
+
+	run_program(
+			"existing int int_test_field; "
+			"existing float float_test_field;"
+			"float_test_field = 12.5;"
+			"int_test_field = 5 + float_test_field; ");
+	ck_assert_int_eq(bx_test_field_get_int(&int_test_field), 17);
+} END_TEST
+
 Suite *test_compiler_create_suite(void) {
 	Suite *suite = suite_create("bx_linked_list");
 	TCase *tcase;
@@ -165,6 +210,10 @@ Suite *test_compiler_create_suite(void) {
 
 	tcase = tcase_create("assignment_test");
 	tcase_add_test(tcase, assignment_test);
+	suite_add_tcase(suite, tcase);
+
+	tcase = tcase_create("expression_test");
+	tcase_add_test(tcase, expression_test);
 	suite_add_tcase(suite, tcase);
 
 	return suite;
